cce1207d1182177b3fd3a9490f2c045e2f9ed96f
User <pedro.anunziato.simoes@gmail.com>
2024-07-19 09:15:48 -0300

    Project Created

---
+++ src/main/java/com/ProjetoSpring/CursoSpring/CursoSpringApplication.java
@@ -0,0 +1,15 @@
+package com.ProjetoSpring.CursoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringApplication.class, args);
+		
+		
+	}
+
+}
