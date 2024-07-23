f330c83df8aaa87c5cb591c846784b5e2db4832c
User <pedro.anunziato.simoes@gmail.com>
2024-07-23 09:05:26 -0300

    Adição da entidade Category

---
+++ src/main/java/com/ProjetoSpring/CursoSpring/services/CategoryService.java
@@ -0,0 +1,29 @@
+package com.ProjetoSpring.CursoSpring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import com.ProjetoSpring.CursoSpring.entities.Category;
+import com.ProjetoSpring.CursoSpring.repositories.CategoryRepository;
+
+import jakarta.websocket.server.ServerEndpoint;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
