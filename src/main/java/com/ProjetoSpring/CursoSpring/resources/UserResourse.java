ac22fb792f3640bd2226dac5b687d0d2181314dc
User <pedro.anunziato.simoes@gmail.com>
2024-07-24 09:35:55 -0300

    Deleção de usuário

--- src/main/java/com/ProjetoSpring/CursoSpring/resources/UserResourse.java
+++ src/main/java/com/ProjetoSpring/CursoSpring/resources/UserResourse.java
@@ -12,6 +12,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.ProjetoSpring.CursoSpring.entities.User;
 import com.ProjetoSpring.CursoSpring.services.UserService;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,11 +37,17 @@ public class UserResourse {
 		return ResponseEntity.ok().body(obj);
 	}
 	
-	@PostMapping(value = "")
+	@PostMapping
 	public ResponseEntity<User> insert(@RequestBody User obj){
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
